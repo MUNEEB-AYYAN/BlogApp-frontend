912c6b37f6ca83d5b7dc21b59c74334c97259ec2
MUNEEB-AYYAN <muneebayyan348@gmail.com>
2025-10-19 03:53:02 +0530

    vercel file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom'
+import Blogs from './pages/Blogs'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path='/' element={<Blogs />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        
+      </Routes>
     </div>
   )
 }
