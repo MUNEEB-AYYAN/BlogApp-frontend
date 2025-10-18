6fdb65894f4d52a683c8993f98c6b05077a4fb1c
MUNEEB-AYYAN <muneebayyan348@gmail.com>
2025-10-19 03:15:23 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box,AppBar,Toolbar,Button, Typography } from '@mui/material'
+
+const Header = () => {
+  return (
+    <AppBar position='sticky'>
+        <Toolbar>
+            <Typography variant='h4'>
+                My Blog App
+            </Typography>
+            <Box 
+            className='flex ml-[auto]'
+            display={'flex'} marginLeft='auto'
+            >
+                <Button sx={{margin:1,color:'white'}}>Login</Button>
+                <Button sx={{margin:1,color:'white'}}>Register</Button>
+                <Button sx={{margin:1,color:'white'}}>Logout</Button>
+            </Box>
+        </Toolbar>
+
+    </AppBar>
+  )
+}
+
+export default Header
